a1a17f9816ec54a1b9a8111ac7e5ad4edd561959
hoangtrung225 <hoangtrung225@gmail.com>
2016-10-27 09:11:30 +0000

    new branch

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
 int main(int argc, char *argv[]) {
-    return 0;
+  char* errorMessage = "usage: wc filename";
+  if(argc != 2)
+  {
+    printf("%s", errorMessage);
+    return 1;
+  }
+  FILE* file = fopen(argv[1],"r");
+  if(file == NULL)
+  {
+    printf("cant open file %s", errorMessage);
+    return 1;
+  }
+  char readChar;
+  int counter = 0;
+  do {
+    /* code */
+    readChar = fgetc(file);
+    counter++;
+  }
+  while(readChar != EOF);
+  printf("character in file: %d", counter -1);
+  return 0;
 }
